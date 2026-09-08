152b1ea8a134af99c17a502be46d76250f2d8706
MariaAcosta1 <L25550705@chihuahua2.tecnm.mx>
2026-09-08 16:50:11 -0600

    Add files via upload

---
+++ EVA1_6_ARREGLOS/EVA1_6_ARREGLOS.java
@@ -0,0 +1,14 @@
+
+package eva1_6_arreglos;
+
+public class EVA1_6_ARREGLOS {
+
+    public static void main(String[] args) {
+        int[] datos = new int[1000000000];//4GB
+        System.out.println(datos);
+        for(int i = 0; i < datos.length; i++){
+            datos[i] = (int)(Math.random() * 100);
+        }
+    }
+    
+}
